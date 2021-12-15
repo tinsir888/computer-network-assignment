fadcec81832d52c9a2eaf409f834e5836bf0e4b7
tinsir888 <912658865@qq.com>
2021-12-16 00:14:01 +0800

    Add files via upload

---
+++ reliableudp.h
@@ -0,0 +1,31 @@
+#pragma comment(lib, "Ws2_32.lib")
+#include<iostream>
+#include<WinSock2.h>
+#include<string>
+#include<string.h>
+#include<time.h>
+#include<fstream>
+#include<stdio.h>
+#include<vector>
+#include<thread>
+#include<mutex>
+using namespace std;
+const int timeout = 300;//超时设定
+const int datalen = 16000;//数据包数据长度
+const int pkgheaderlen = 6;//数据包头部长度
+const int pkglength = 16006;//数据包长度
+const int maxn = 2e8;//内存中最大缓存数据包个数
+SOCKADDR_IN serverAddr, clientAddr;
+unsigned char checksum(char* ch, int len)
+{
+	if (len == 0) return 0xff;
+	unsigned char cksum = ch[0];
+	for (int i = 1; i < len; i++)
+	{
+		unsigned int tmp = cksum + (unsigned char)ch[i];
+		tmp = (tmp >> 8) + (tmp & 0xff);
+		tmp = (tmp >> 8) + (tmp & 0xff);
+		cksum = tmp;
+	}
+	return ~cksum;
+}
