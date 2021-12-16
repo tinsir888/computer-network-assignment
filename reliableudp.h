50bcc269a675feb5eb28c72bb4e500bdc6c9705c
tinsir888 <912658865@qq.com>
2021-12-16 20:22:12 +0800

    Add files via upload

--- reliableudp.h
+++ reliableudp.h
@@ -10,10 +10,11 @@
 #include<thread>
 #include<mutex>
 using namespace std;
-const int timeout = 300;//超时设定
-const int datalen = 16000;//数据包数据长度
+SOCKET server, client;
+const int timeout = 200;//超时设定
+const int datalen = 14994;//数据包数据长度
 const int pkgheaderlen = 6;//数据包头部长度
-const int pkglength = 16006;//数据包长度
+const int pkglength = 15000;//数据包长度
 const int maxn = 2e8;//内存中最大缓存数据包个数
 SOCKADDR_IN serverAddr, clientAddr;
 unsigned char checksum(char* ch, int len)
